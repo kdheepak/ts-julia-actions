3a3055df251e6642b5905620dba7866c75988e74
Dheepak Krishnamurthy <me@kdheepak.com>
2024-06-09 05:49:00 -0400

    feat: Add LongToShort and Toggle (#1)

--- lua/ts-julia-actions.lua
+++ lua/ts-julia-actions.lua
@@ -29,17 +29,24 @@ function M.setup(opts)
   vim.api.nvim_create_user_command(
     "JuliaShortToLongFunctionAction",
     M.short_to_long_function,
-    { desc = "Performs short to long function action on the node under the cursor." }
+    { desc = "Performs short to long function action on the TS node under the cursor." }
+  )
+
+  vim.api.nvim_create_user_command(
+    "JuliaLongToShortFunctionAction",
+    M.long_to_short_function,
+    { desc = "Performs long to short function action on the TS node under the cursor." }
+  )
+
+  vim.api.nvim_create_user_command(
+    "JuliaToggleFunctionDefinitionAction",
+    M.toggle_function_definition,
+    { desc = "Performs toggle function Definition action on the TS node under the cursor." }
   )
 end
 
-local function get_node_text(bufnr, node)
-  local start_row, start_col, end_row, end_col = node:range()
-  if start_row ~= end_row then
-    return nil
-  end
-  local line = vim.api.nvim_buf_get_lines(bufnr, start_row, start_row + 1, false)[1]
-  return line:sub(start_col + 1, end_col)
+function M.config(opts)
+  M.setup(opts)
 end
 
 local function split_into_lines(str)
@@ -50,30 +57,125 @@ local function split_into_lines(str)
   return lines
 end
 
-M.short_to_long_function = M.set_repeat(function()
-  local bufnr = vim.api.nvim_get_current_buf()
+local function _get_parent_function_node()
   local node = ts_utils.get_node_at_cursor()
-  -- TODO: currently only works when cursor is on `=` in short_function_definition
-  if node:type() == "short_function_definition" then
-    local function_name_node = node:named_child(0) -- Replace with correct child index
-    local parameters_node = node:named_child(1) -- Replace with correct child index
-    local body_node = node:named_child(2) -- Replace with correct child index
+  while node do
+    if node:type() == "function_definition" then
+      break
+    else
+      node = node:parent()
+    end
+  end
+  if node == nil then
+    return nil
+  end
 
-    local function_name = get_node_text(bufnr, function_name_node)
-    local parameters = get_node_text(bufnr, parameters_node)
-    local body = get_node_text(bufnr, body_node)
+  return node
+end
 
-    -- Prepare the transformed function
-    -- TODO: preserve indentation of original function
-    local transformed = string.format("function %s%s\n  %s\nend", function_name, parameters, body)
-    local transformed_lines = split_into_lines(transformed)
+M.long_to_short_function = M.set_repeat(function()
+  local bufnr = vim.api.nvim_get_current_buf()
 
-    -- Replace the function definition in the buffer with the transformed version
-    local start_row, start_col, end_row, end_col = node:range()
-    vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, transformed_lines)
+  local function_node = _get_parent_function_node()
+
+  if function_node then
+    local call_expr = vim.treesitter.get_node_text(function_node:named_child(0), bufnr)
+    if function_node:named_child_count() > 2 then
+      local children = ts_utils.get_named_children(function_node)
+      local transformed = ""
+      for i, child in ipairs(children) do
+        if i ~= 1 then
+          transformed = string.format("%s\n    %s", transformed, vim.treesitter.get_node_text(child, bufnr))
+        end
+      end
+      transformed = string.format("%s = begin\n%s\nend", call_expr, transformed)
+      local transformed_lines = split_into_lines(transformed)
+      local start_row, start_col, end_row, end_col = function_node:range()
+      vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, transformed_lines)
+    else
+      local children = ts_utils.get_named_children(function_node)
+      local transformed = ""
+      for i, child in ipairs(children) do
+        if i ~= 1 then
+          transformed = string.format("%s", vim.treesitter.get_node_text(child, bufnr))
+        end
+      end
+      transformed = string.format("%s = %s", call_expr, transformed)
+      local transformed_lines = split_into_lines(transformed)
+      local start_row, start_col, end_row, end_col = function_node:range()
+      vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, transformed_lines)
+    end
   else
-    print("The current node is not a short function definition.")
+    vim.notify("JuliaLongToShortFunctionAction can only be performed on long functions.", vim.log.levels.ERROR)
   end
 end)
+
+local function _get_parent_assignment_node(bufnr)
+  local node = ts_utils.get_node_at_cursor()
+  while node do
+    if node:type() == "assignment" then
+      local op_node = node:named_child(1)
+      if
+        op_node
+        and vim.treesitter.get_node_text(op_node, bufnr) == "="
+        and node:named_child(0):type() == "call_expression"
+      then
+        break
+      else
+        node = node:parent()
+      end
+    else
+      node = node:parent()
+    end
+  end
+  if node == nil then
+    return nil
+  end
+
+  return node
+end
+
+M.short_to_long_function = M.set_repeat(function()
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  local assignment_node = _get_parent_assignment_node(bufnr)
+
+  if assignment_node then
+    local call_expr = vim.treesitter.get_node_text(assignment_node:named_child(0), bufnr)
+    local body = vim.treesitter.get_node_text(assignment_node:named_child(2), bufnr)
+
+    if vim.startswith(body, "begin") then
+      local compound_statement = assignment_node:named_child(2)
+      local children = ts_utils.get_named_children(compound_statement)
+      local transformed_lines = { string.format("function %s", call_expr) }
+      for i, child in ipairs(children) do
+        transformed_lines[#transformed_lines + 1] = string.format("    %s", vim.treesitter.get_node_text(child, bufnr))
+      end
+      transformed_lines[#transformed_lines + 1] = "end"
+      -- Replace the function definition in the buffer with the transformed version
+      local start_row, start_col, end_row, end_col = assignment_node:range()
+      vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, transformed_lines)
+    else
+      local transformed = string.format("function %s\n  %s\nend", call_expr, body)
+      local transformed_lines = split_into_lines(transformed)
+
+      -- Replace the function definition in the buffer with the transformed version
+      local start_row, start_col, end_row, end_col = assignment_node:range()
+      vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, transformed_lines)
+    end
+  else
+    vim.notify("JuliaShortToLongFunctionAction can only be performed on short functions.", vim.log.levels.ERROR)
+  end
+end)
+
+M.toggle_function_definition = function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local assignment_node = _get_parent_assignment_node(bufnr)
+  if assignment_node then
+    M.short_to_long_function()
+  else
+    M.long_to_short_function()
+  end
+end
 
 return M
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,13 @@
 foo(x) = x + 1
+
+foo(x) = x[] = 1
+
+function foo(x)
+  x + 1
+end
+
+foo(x) = begin
+    println("hello")
+    x + 1
+end
+
