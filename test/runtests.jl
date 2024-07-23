b91c9506f45127b6ac50590a5137c1939a42036c
Dheepak Krishnamurthy <noreply@kdheepak.com>
2024-07-23 06:14:13 -0400

    feat: Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,37 @@ foo(x) = begin
     println("hello")
     x + 1
 end
+
+markdown = md"""
+# Header 1
+
+## Header 2
+
+### Header 3
+
+1. List element 1
+2. Syntax highlighting for inline `code`
+3. **Bold** and _Italic_
+
+```julia
+function test()
+println("hello world")
+end
+```
+
+```rust
+fn test() {
+println("hello world")
+}
+```
+"""
+
+function test()
+
+    run(`git status`)
+
+    run(`git commit -am "feat: syntax highlighting in commands"`)
+
+    run(`git push`)
+
+end
